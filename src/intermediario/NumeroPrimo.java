e722f0710cc185ae9fcc48bdd6831cce1e37bd0c
Isaque_Soares <isaquesbl123@gmail.com>
2024-12-22 22:56:26 -0300

    Solved exercise to find out if a number is prime

--- src/intermediario/NumeroPrimo.java
+++ src/intermediario/NumeroPrimo.java
@@ -1,7 +1,43 @@
+/* 
+    Descrição: Faça um programa que verifique se um número fornecido pelo
+    usuário é primo.
+    Objetivo: Praticar loops e lógica condicional
+*/
 
+import java.util.Scanner;
 
 public class NumeroPrimo {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número para verificar se é primo: ");
+        int num = sc.nextInt();
+
+        if (ehPrimo(num)) {
+            System.out.println(num + " é um número primo.");
+        } else {
+            System.out.println(num + " não é um número primo.");
+        }
+
+        sc.close();
+    }
+
+    public static boolean ehPrimo(int num) {
+        if (num <= 1) {
+            return false; 
+        }
+        if (num == 2) {
+            return true; 
+        }
+        if (num % 2 == 0) {
+            return false; 
+        }
+
+        for (int i = 3; i <= Math.sqrt(num); i += 2) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true; 
     }
 }
